501f3a95ead45add2bd4c5d80c425e8bfea27bd2
earbili <earbili@EARBILI-MOBL1.ger.corp.intel.com>
2013-09-12 00:39:01 +0300

    remove unused imports in database class

--- Pain Recognition Brain Side/src/dataLayer/DataBase.java
+++ Pain Recognition Brain Side/src/dataLayer/DataBase.java
@@ -1,24 +1,11 @@
 package dataLayer;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Vector;
-
-import javax.swing.JOptionPane;
 
 public class DataBase {
 	public static Connection conn ;
