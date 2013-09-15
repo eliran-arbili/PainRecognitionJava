8854ce520885ceb17886bc8c64203610cbedc2ef
ariegaon <gaon.arie@gmail.com>
2013-09-16 00:50:12 +0300

    Add Fuzzyfication function

--- Pain Recognition Brain Side/src/businessLogic/CBRController.java
+++ Pain Recognition Brain Side/src/businessLogic/CBRController.java
@@ -7,4 +7,21 @@ public class CBRController {
 		 * TO-DO: complete implementation
 		 */
 	}
+	public double[] fuzzification(double [] actionUnits)
+	{
+		double rangeNum=1;
+		int rangeSize=10;
+		double error=0.01; 
+		double [] fuzzy=new double[rangeSize];
+		double factor=rangeNum/rangeSize;
+		for(int i=0;i<rangeSize;i++)
+			fuzzy[i]=fuzzy[i]+factor;
+		for(int i=0;i<actionUnits.length;i++)
+		{
+			int index=(int)Math.round((actionUnits[i]-error)/factor);
+			actionUnits[i]=(index>0)?index-1:0;
+		}
+		
+		return actionUnits;
+	}
 }
