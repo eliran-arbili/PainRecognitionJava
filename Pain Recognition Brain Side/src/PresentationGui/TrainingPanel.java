95f814422ea2c2f521dfc973a5a82d9a2c7df89d
eliran-arbili <eliranarbeli@gmail.com>
2013-12-09 22:54:52 +0200

    Improved GUI consistency, look&feel and backgrounds.

--- Pain Recognition Brain Side/src/PresentationGui/TrainingPanel.java
+++ Pain Recognition Brain Side/src/PresentationGui/TrainingPanel.java
@@ -1,19 +1,13 @@
 package PresentationGui;
 
-import java.awt.EventQueue;
-
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-
 import org.encog.engine.network.activation.ActivationSigmoid;
 import org.encog.engine.network.activation.ActivationTANH;
-
 import dataLayer.ProjectConfig;
 import businessLogic.ProjectUtils;
 import businessLogic.training.NeuralTrainDesciptor;
 import businessLogic.training.TrainingSession;
 import businessLogic.training.TrainingSession.ConfKeys;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
@@ -23,168 +17,149 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 
 
 @SuppressWarnings("serial")
-public class TrainingGui extends JDialog {
+public class TrainingPanel extends BackgroundPanel {
 
 	
+	/*
+	 * Instance Variables
+	 */
 	private MouseListener tableClickListener;
 	private TrainingSession currentTrainSession;
-	private String trainingTag;
 	private File dataSetFileToUse;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TrainingGui dialog = new TrainingGui("sample");
-					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-					dialog.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-	}
 
 	/*
 	 * Constructors
 	 */
 
-	public TrainingGui(String trainingTag) {
-		this.trainingTag = trainingTag;
+	public TrainingPanel(Image image) {
+		super(image);
 		initialize();
 	}
-
-	protected void onClickBrowse(ActionEvent e) {
-		JFileChooser jfc = new JFileChooser(ProjectConfig.DATASETS_PATH);
-		jfc.setFileFilter(new CSVFileFilter());
-		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		int retval = jfc.showOpenDialog(this);
-		if(retval == JOptionPane.YES_OPTION){
-			textFieldDataSet.setText(jfc.getSelectedFile().getAbsolutePath());
-			tableTrainedNetworks.removeAll();
-		}
-	}
 	
-	private boolean isFieldsOK()
-	{
-		
-		for (java.awt.Component cmp : this.getContentPane().getComponents()) {
-			if(cmp instanceof JTextField){
-		        if(((JTextField)cmp).getText().isEmpty()){
-		        	JOptionPane.showMessageDialog(this, "Please fill empty fields", "Operation couldn't be completed", JOptionPane.INFORMATION_MESSAGE);
-					return false;
-		        }
-			}
-	    }
-		return true;	
-	}
 	
 	private void initialize(){
-		setBounds(100, 100, 575, 500);
-		btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(120, 398, 100, 34);
+		setBounds(100, 100, 580, 460);
 		btnTrain = new JButton("Train");
-		btnTrain.setBounds(310, 398, 100, 34);
+		btnTrain.setFont(new Font("Arial", Font.BOLD, 14));
+		btnTrain.setBounds(310, 403, 100, 34);
 		lblDataSet = new JLabel("Data Set:");
-		lblDataSet.setBounds(60, 24, 100, 14);
+		lblDataSet.setFont(new Font("Arial", Font.BOLD, 14));
+		lblDataSet.setBounds(10, 29, 140, 14);
 		lblActivationFunction = new JLabel("Activation Function:");
-		lblActivationFunction.setBounds(10, 52, 130, 14);
+		lblActivationFunction.setFont(new Font("Arial", Font.BOLD, 14));
+		lblActivationFunction.setBounds(10, 60, 140, 14);
 		lblTrainingType = new JLabel("Training Type:");
-		lblTrainingType.setBounds(37, 116, 120, 14);
+		lblTrainingType.setFont(new Font("Arial", Font.BOLD, 14));
+		lblTrainingType.setBounds(10, 122, 140, 14);
 		lblNetworkType = new JLabel("Network Type:");
-		lblNetworkType.setBounds(35, 85, 120, 14);
+		lblNetworkType.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNetworkType.setBounds(10, 91, 140, 14);
 		lblMaxEpochs = new JLabel("Max Epochs:");
-		lblMaxEpochs.setBounds(45, 147, 120, 14);
+		lblMaxEpochs.setFont(new Font("Arial", Font.BOLD, 14));
+		lblMaxEpochs.setBounds(10, 153, 140, 14);
 		lbStripLength = new JLabel("Strip Length:");
-		lbStripLength.setBounds(44, 178, 120, 14);
+		lbStripLength.setFont(new Font("Arial", Font.BOLD, 14));
+		lbStripLength.setBounds(10, 184, 140, 14);
 		lblAlpha = new JLabel("Alpha:");
-		lblAlpha.setBounds(75, 240, 100, 14);
+		lblAlpha.setFont(new Font("Arial", Font.BOLD, 14));
+		lblAlpha.setBounds(10, 246, 140, 14);
 		lblMinEfficiency = new JLabel("Min Efficiency:");
-		lblMinEfficiency.setBounds(37, 209, 110, 14);
+		lblMinEfficiency.setFont(new Font("Arial", Font.BOLD, 14));
+		lblMinEfficiency.setBounds(10, 215, 140, 14);
 		lblInputNeurons = new JLabel("Input Neurons:");
-		lblInputNeurons.setBounds(33, 365, 120, 14);
+		lblInputNeurons.setFont(new Font("Arial", Font.BOLD, 14));
+		lblInputNeurons.setBounds(10, 370, 140, 14);
 		lblOutputNeurons = new JLabel("Output Neurons:");
-		lblOutputNeurons.setBounds(25, 303, 120, 14);
+		lblOutputNeurons.setFont(new Font("Arial", Font.BOLD, 14));
+		lblOutputNeurons.setBounds(10, 308, 140, 14);
 		lblHiddenNeurons = new JLabel("Hidden Neurons:");
-		lblHiddenNeurons.setBounds(26, 334, 120, 14);
+		lblHiddenNeurons.setFont(new Font("Arial", Font.BOLD, 14));
+		lblHiddenNeurons.setBounds(10, 339, 140, 14);
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(216, 147, 343, 236);
+		scrollPane.setBounds(216, 152, 343, 236);
 		chckbxCSVHeaders = new JCheckBox("CSVHeaders",true);
-		chckbxCSVHeaders.setBounds(348, 48, 95, 23);
+		chckbxCSVHeaders.setFont(new Font("Arial", Font.BOLD, 14));
+		chckbxCSVHeaders.setBounds(332, 56, 126, 23);
 
 		
 		textFieldDataSet = new JTextField();
 		textFieldDataSet.setEditable(false);
-		textFieldDataSet.setBounds(140, 21, 300, 20);
+		textFieldDataSet.setBounds(158, 27, 300, 20);
 		textFieldDataSet.setColumns(10);
 
 		btnBrowse = new JButton("Browse");
-		btnBrowse.setBounds(468, 20, 81, 23);
+		btnBrowse.setFont(new Font("Arial", Font.BOLD, 14));
+		btnBrowse.setBounds(468, 25, 91, 23);
 		btnBrowse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				onClickBrowse(e);
 			}
 		});
 		comboBoxActivationFunction = new JComboBox<String>();
-		comboBoxActivationFunction.setBounds(137, 49, 145, 20);
+		comboBoxActivationFunction.setBounds(160, 58, 145, 20);
 		comboBoxActivationFunction.addItem("Sigmoid");
 		comboBoxActivationFunction.addItem("TANH");
 		comboBoxNetworkType = new JComboBox<String>();
 		comboBoxNetworkType.addItem("MLP");
 		comboBoxNetworkType.addItem("RBF");
-		comboBoxNetworkType.setBounds(137, 82, 145, 20);
+		comboBoxNetworkType.setBounds(160, 89, 145, 20);
 
 		comboBoxTrainingType = new JComboBox<String>();
 		comboBoxTrainingType.addItem("Back Propagation");
 		comboBoxTrainingType.addItem("Reasilent Propagation");
-		comboBoxTrainingType.setBounds(137, 113, 145, 20);
+		comboBoxTrainingType.setBounds(160, 120, 145, 20);
 
 		lblKFolds = new JLabel("K-Folds:");
-		lblKFolds.setBounds(67, 271, 100, 14);
+		lblKFolds.setFont(new Font("Arial", Font.BOLD, 14));
+		lblKFolds.setBounds(10, 277, 140, 14);
 
 		textFieldStripLength = new JTextField();
-		textFieldStripLength.setBounds(137, 175, 36, 20);
+		textFieldStripLength.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldStripLength.setBounds(160, 182, 36, 20);
 		textFieldStripLength.setColumns(10);
 		textFieldStripLength.setToolTipText("Typical value: 1");
 
 		textFieldMaxEpochs = new JTextField();
-		textFieldMaxEpochs.setBounds(137, 144, 36, 20);
+		textFieldMaxEpochs.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldMaxEpochs.setBounds(160, 151, 36, 20);
 		textFieldMaxEpochs.setColumns(10);
 		textFieldMaxEpochs.setToolTipText("Typical value: 15,000");
 
 		textFieldMinEffiency = new JTextField();
-		textFieldMinEffiency.setBounds(137, 206, 36, 20);
+		textFieldMinEffiency.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldMinEffiency.setBounds(160, 211, 36, 20);
 		textFieldMinEffiency.setColumns(10);
 		textFieldMinEffiency.setToolTipText("Typical value: 0.1");
 
 		textFieldAlpha = new JTextField();
-		textFieldAlpha.setBounds(137, 237, 36, 20);
+		textFieldAlpha.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldAlpha.setBounds(160, 242, 36, 20);
 		textFieldAlpha.setColumns(10);
 		textFieldAlpha.setToolTipText("Typical value: ");
 
 		textFieldKFolds = new JTextField();
-		textFieldKFolds.setBounds(137, 268, 36, 20);
+		textFieldKFolds.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldKFolds.setBounds(160, 273, 36, 20);
 		textFieldKFolds.setColumns(10);
 
 		textFieldOutputNeurons = new JTextField();
-		textFieldOutputNeurons.setBounds(137, 300, 36, 20);
+		textFieldOutputNeurons.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldOutputNeurons.setBounds(160, 305, 36, 20);
 		textFieldOutputNeurons.setColumns(10);
 
 		textFieldHiddenNeurons = new JTextField();
-		textFieldHiddenNeurons.setBounds(137, 331, 36, 20);
+		textFieldHiddenNeurons.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldHiddenNeurons.setBounds(160, 336, 36, 20);
 		textFieldHiddenNeurons.setColumns(10);
 
 		textFieldInputNeurons = new JTextField();
-		textFieldInputNeurons.setBounds(137, 362, 36, 20);
+		textFieldInputNeurons.setFont(new Font("Arial", Font.PLAIN, 12));
+		textFieldInputNeurons.setBounds(160, 367, 36, 20);
 		textFieldInputNeurons.setColumns(10);
 
 
@@ -194,12 +169,7 @@ public class TrainingGui extends JDialog {
 			}
 		});
 		
-		btnCancel.addActionListener(new ActionListener() {		
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				onClickCancel();
-			}
-		});
+
 		tableTrainedNetworks = new JTable();
 		tableTrainedNetworks.setFillsViewportHeight(true);
 		tableTrainedNetworks.setRowHeight(25);
@@ -229,56 +199,52 @@ public class TrainingGui extends JDialog {
 		image.setImageObserver(iconLabel);
 		iconLabel.setVisible(false);
 		
-		getContentPane().add(iconLabel);
-		getContentPane().setLayout(null);
-		getContentPane().add(btnCancel);
-		getContentPane().add(lblAlpha);
-		getContentPane().add(lblMinEfficiency);
-		getContentPane().add(lbStripLength);
-		getContentPane().add(lblActivationFunction);
-		getContentPane().add(lblNetworkType);
-		getContentPane().add(lblTrainingType);
-		getContentPane().add(lblMaxEpochs);
-		getContentPane().add(comboBoxActivationFunction);
-		getContentPane().add(textFieldMaxEpochs);
-		getContentPane().add(textFieldStripLength);
-		getContentPane().add(textFieldMinEffiency);
-		getContentPane().add(textFieldAlpha);
-		getContentPane().add(comboBoxNetworkType);
-		getContentPane().add(comboBoxTrainingType);
-		getContentPane().add(lblOutputNeurons);
-		getContentPane().add(lblInputNeurons);
-		getContentPane().add(lblDataSet);
-		getContentPane().add(lblHiddenNeurons);
-		getContentPane().add(lblKFolds);
-		getContentPane().add(textFieldKFolds);
-		getContentPane().add(textFieldOutputNeurons);
-		getContentPane().add(textFieldHiddenNeurons);
-		getContentPane().add(textFieldInputNeurons);
-		getContentPane().add(btnTrain);
-		getContentPane().add(textFieldDataSet);
-		getContentPane().add(btnBrowse);
-		getContentPane().add(chckbxCSVHeaders);
-		getContentPane().add(scrollPane);
-		getContentPane().setBackground(Color.CYAN);
-		this.setResizable(false);
+		this.add(iconLabel);
+		this.setLayout(null);
+		this.add(lblAlpha);
+		this.add(lblMinEfficiency);
+		this.add(lbStripLength);
+		this.add(lblActivationFunction);
+		this.add(lblNetworkType);
+		this.add(lblTrainingType);
+		this.add(lblMaxEpochs);
+		this.add(comboBoxActivationFunction);
+		this.add(textFieldMaxEpochs);
+		this.add(textFieldStripLength);
+		this.add(textFieldMinEffiency);
+		this.add(textFieldAlpha);
+		this.add(comboBoxNetworkType);
+		this.add(comboBoxTrainingType);
+		this.add(lblOutputNeurons);
+		this.add(lblInputNeurons);
+		this.add(lblDataSet);
+		this.add(lblHiddenNeurons);
+		this.add(lblKFolds);
+		this.add(textFieldKFolds);
+		this.add(textFieldOutputNeurons);
+		this.add(textFieldHiddenNeurons);
+		this.add(textFieldInputNeurons);
+		this.add(btnTrain);
+		this.add(textFieldDataSet);
+		this.add(btnBrowse);
+		this.add(chckbxCSVHeaders);
+		this.add(scrollPane);
 	}
 
-	protected void onClickCancel() {
-		dispose();
+	/*
+	 * Components callback's
+	 */
+	protected void onClickBrowse(ActionEvent e) {
+		JFileChooser jfc = new JFileChooser(ProjectConfig.DATASETS_PATH);
+		jfc.setFileFilter(new CSVFileFilter());
+		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		int retval = jfc.showOpenDialog(this);
+		if(retval == JOptionPane.YES_OPTION){
+			textFieldDataSet.setText(jfc.getSelectedFile().getAbsolutePath());
+			tableTrainedNetworks.removeAll();
+		}
 	}
-
-	private void createTag() throws IOException{
-		File tagDir = null;
-		NeuralTrainDesciptor selectedNet = ((TrainingTableModel)tableTrainedNetworks.getModel()).getItemAtRow(tableTrainedNetworks.convertRowIndexToModel(tableTrainedNetworks.getSelectedRow()));
-		tagDir = new File(ProjectUtils.combine(ProjectConfig.TRAINING_TAGS_PATH, trainingTag));
-		tagDir.mkdir();
-		selectedNet.saveAsEncogFile(tagDir.getAbsolutePath());
-		File dataSetFileDest 	= new File(ProjectUtils.combine(tagDir.getAbsolutePath(),dataSetFileToUse.getName()));
-		Files.copy(dataSetFileToUse.toPath(),dataSetFileDest.toPath());
-		dataSetFileToUse.delete(); // Delete after copy to tag dir
-	}
-
+	
 	protected void onClickTrain(ActionEvent e) {
 		if(! isFieldsOK()){
 			return;
@@ -339,21 +305,69 @@ public class TrainingGui extends JDialog {
 		if(e.getClickCount() == 2 && ! e.isConsumed()){
 			e.consume();
 			int rowPressed = ((JTable) e.getSource()).rowAtPoint(e.getPoint());
-			if (rowPressed == ((JTable) e.getSource()).getSelectedRow()) {
-				int retval = JOptionPane.showConfirmDialog(this, "This Will Create New Neural Network tag\nPress OK To Continue","New Tag",JOptionPane.OK_CANCEL_OPTION);
-				if(retval == JOptionPane.OK_OPTION){
-					try
-					{
-						createTag();
-						dispose();
-					}
-					catch(IOException ex)
-					{
-						JOptionPane.showMessageDialog(this, ex.getMessage(), "Operation Couldn't Be Completed", JOptionPane.ERROR_MESSAGE);
-					}
-				}
+			if (rowPressed != ((JTable) e.getSource()).getSelectedRow()) {
+				return;
+			}
+			String tagName = JOptionPane.showInputDialog(this, "Please Enter Training Tag Name","New Tag",JOptionPane.OK_CANCEL_OPTION);
+			if(tagName == null){
+				return;
+			}
+			if(! isTagValid(tagName)){
+				JOptionPane.showMessageDialog(this, "Invalid file name or it already exists", "Invalid Tag Name", JOptionPane.ERROR_MESSAGE);
+				return ;
+			}
+			try
+			{
+				createTag(tagName);
+			}
+			catch(IOException ex)
+			{
+				JOptionPane.showMessageDialog(this, ex.getMessage(), "Operation Couldn't Be Completed", JOptionPane.ERROR_MESSAGE);
+			}
+			JOptionPane.showMessageDialog(this, "Training Tag Successfuly Created!", "Training", JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+	
+	
+	private boolean isFieldsOK()
+	{
+		
+		for (java.awt.Component cmp : this.getComponents()) {
+			if(cmp instanceof JTextField){
+		        if(((JTextField)cmp).getText().isEmpty()){
+		        	JOptionPane.showMessageDialog(this, "Please fill empty fields", "Operation couldn't be completed", JOptionPane.WARNING_MESSAGE);
+					return false;
+		        }
+			}
+	    }
+		return true;	
+	}
+
+	private void createTag(String trainingTag) throws IOException{
+		File tagDir = null;
+		NeuralTrainDesciptor selectedNet = ((TrainingTableModel)tableTrainedNetworks.getModel()).getItemAtRow(tableTrainedNetworks.convertRowIndexToModel(tableTrainedNetworks.getSelectedRow()));
+		tagDir = new File(ProjectUtils.combine(ProjectConfig.TRAINING_TAGS_PATH, trainingTag));
+		tagDir.mkdir();
+		selectedNet.saveAsEncogFile(tagDir.getAbsolutePath());
+		File dataSetFileDest 	= new File(ProjectUtils.combine(tagDir.getAbsolutePath(),dataSetFileToUse.getName()));
+		Files.copy(dataSetFileToUse.toPath(),dataSetFileDest.toPath());
+		dataSetFileToUse.delete(); // Delete after copy to tag dir
+	}
+
+	
+	private boolean isTagValid(String tag) {
+		File tagDir = new File(ProjectUtils.combine(ProjectConfig.TRAINING_TAGS_PATH, tag));
+		try{
+			java.nio.file.Path tagPath = tagDir.toPath();
+			if(Files.exists(tagPath)){
+				return false;
 			}
 		}
+		catch(java.nio.file.InvalidPathException ex)
+		{
+			return false;
+		}
+		return true;
 	}
 		
 
@@ -390,7 +404,6 @@ public class TrainingGui extends JDialog {
 	private JTextField textFieldOutputNeurons;
 	private JTextField textFieldHiddenNeurons;
 	private JTextField textFieldInputNeurons;
-	private JButton btnCancel;
 	private JButton btnBrowse;
 	private JButton btnTrain;
 	private JLabel lblDataSet;
