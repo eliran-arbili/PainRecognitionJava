92a50bdf6dc6d8c328346a438e413bcb886ede6a
earbili <earbili@EARBILI-MOBL1.ger.corp.intel.com>
2014-01-02 14:49:07 +0200

    added documentation for tracker process

--- Pain Recognition Brain Side/src/businessLogic/TrackerProcess.java
+++ Pain Recognition Brain Side/src/businessLogic/TrackerProcess.java
@@ -3,18 +3,38 @@ package businessLogic;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Wrapper for the tracking process program
+ * This class can represent the client side process. 
+ * Activating the client process is done in a different thread.
+ * @author Eliran Arbili , Arie Gaon
+ */
 public class TrackerProcess {
 	
+	/*
+	 * Instnace variables
+	 */
 	private Thread processWrapper;
 	private Process vendorTracker;
 	private File trackerProgram;
 	private boolean alive;
 
+	/**
+	 * Create new Tracker process
+	 * @param trackerProgram- executable program file
+	 */
 	public TrackerProcess(File trackerProgram){
 		this.trackerProgram = trackerProgram;
 		alive = false;
 	}
 	
+	/*
+	 * Member functions
+	 */
+	
+	/**
+	 * Start the tacker program in different thread
+	 */
 	public void start(){
 		processWrapper = new Thread() {	
 			@Override
@@ -24,6 +44,28 @@ public class TrackerProcess {
 		};
 		processWrapper.start();
 	}
+	
+	/**
+	 * kill the tracker process
+	 */
+	public void killTracker(){
+		if(vendorTracker != null){
+			vendorTracker.destroy();
+		}
+
+	}
+
+	/**
+	 *  check if program still runs
+	 * @return true if program still run, else false
+	 */
+	public boolean isAlive() {
+		return alive;
+	}
+	
+	/*
+	 * Auxiliary Methods
+	 */
 	private void runProcess() {
 		ProcessBuilder pb = new ProcessBuilder(trackerProgram.getAbsolutePath());
 		try {
@@ -36,16 +78,6 @@ public class TrackerProcess {
 		}
 	}
 	
-	public void killTracker(){
-		if(vendorTracker != null){
-			vendorTracker.destroy();
-		}
-
-	}
-
 	
-	public boolean isAlive() {
-		return alive;
-	}
 
 }
